90c5756b71fe0586d8dcde768adf6a71b055b5b4
yaacov <david@example.com>
2023-11-13 11:40:58 +0200

    all

--- client/src/component/functions.tsx
+++ client/src/component/functions.tsx
@@ -1,23 +1,23 @@
 import { useAppDispatch } from "../app/hooks";
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
 import { setProducts } from "../app/dataSlice";
 
-export function connectToData(){
+export function connectToData() {
 
-    const dispatch = useAppDispatch()
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(
-              "http://localhost:8181/api/products"
-            );
-            dispatch(setProducts(response.data ));
-          } catch (error) {
-            console.error(error);
-          }
-        };
-        fetchData();
-      }, []);
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8181/api/products"
+        );
+        dispatch(setProducts(response.data));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
 }
