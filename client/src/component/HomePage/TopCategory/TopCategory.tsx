d7dd4aa22c53103f0ab6e5f0ff2719798f541b99
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-13 10:58:19 +0200

    drop data branch push to main

--- client/src/component/HomePage/TopCategory/TopCategory.tsx
+++ client/src/component/HomePage/TopCategory/TopCategory.tsx
@@ -1,10 +1,12 @@
+import { getBindingIdentifiers } from "@babel/types";
 import { useAppSelector } from "../../../app/hooks";
 import Product from "../../interface";
 
 export default function TopCategoryAndProduct() {
   
-    const data = useAppSelector((state) => state.products);
-    
+    // const data = useAppSelector((state) => state.products);
+    // const topCategory = getBindingIdentifiers(data.products)
+    // console.log(topCategory);
     return( 
     <div>
 
